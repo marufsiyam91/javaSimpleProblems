5f7372b99b691d1ba5e2960c18f64744a2ebb311
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-22 23:08:00 +0600

    print rombos through for loop

--- src/com/Problems/Rombos.java
+++ src/com/Problems/Rombos.java
@@ -2,6 +2,17 @@ package com.Problems;
 
 public class Rombos {
     public static void main(String[] args) {
+        int n = 7;
 
+        for (int i = 1; i <= n; i++){
+            int spaces = n - i;
+            for(int j = 1; j <= spaces; j++){
+                System.out.print("  ");
+            }
+            for(int j = 1; j <= n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
