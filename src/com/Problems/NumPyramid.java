2e3c21ca8cd8a6ba7c6c09b9d09876d91e17af43
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-22 23:21:42 +0600

    printed number pyramid with for loop

---
+++ src/com/Problems/NumPyramid.java
@@ -0,0 +1,22 @@
+package com.Problems;
+
+public class NumPyramid {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            int spaces = n - i;
+            for (int j = 1; j <= spaces; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print(i);
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
